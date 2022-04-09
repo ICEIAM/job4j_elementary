85523d1b77f5c4a5c039dadd7b99c4d178730ec2
ICEIAM <dmitriy.lukkin@gmail.com>
2022-04-09 23:22:29 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Diagnosis.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Diagnosis {
+    private String diagnosis;
+
+    public String diagnosis() {
+        this.diagnosis = diagnosis;
+        return this.diagnosis;
+    }
+}
